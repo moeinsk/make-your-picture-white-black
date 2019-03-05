a7a0365d3afa2505b4841ddd21a0068306904b2a
i am moein <44437189+moeinsk@users.noreply.github.com>
2019-03-05 04:27:54 -0800

    Update black with.py

--- black with.py
+++ black with.py
@@ -1,3 +1,5 @@
+#writed by moein
 from PIL import Image
-img = Image.open("python-powered-w-140x56.png")
+img = Image.open("your originally pic")
 img.convert('L').save('new pik.png')
+#thise is python's power
